d9ff4c82a539ea1bf0e915a9e5c55695405244bc
Paulo <pauloh16.pdr@gmail.com>
2024-02-27 18:57:35 -0300

    configurar login com google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,60 @@
 import React from "react";
 
-import BackgroundImage from '../../assets/background.png';
+import { Alert } from "react-native";
+
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+
+import BackgroundImage from "../../assets/background.png";
 import Button from "../../components/Button";
 
-import * as Styles from './styles';
+import * as Styles from "./styles";
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
 
 const SignIn: React.FC = () => {
-  return <Styles.Container source={BackgroundImage}>
-    <Styles.Title>
-      Ignite Fleet
-    </Styles.Title>
+  const [isAuthenticating, setIsAuthenticating] = React.useState(false);
 
-    <Styles.Slogan>
-      Gestão de uso de veículos
-    </Styles.Slogan>
+  const handleGoolgeSignin = async () => {
+    try {
+      setIsAuthenticating(true);
 
-    <Button title="Entrar com Google" />
-  </Styles.Container>;
+      const { idToken } = GoogleSignin.signIn();
+
+      if (idToken) {
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+      }
+    } catch (e) {
+      console.error(e);
+
+      Alert.alert("Entrar", "Não foi possível conecta-se a sua conta Google.");
+    } finally {
+      setIsAuthenticating(false);
+    }
+  };
+
+  return (
+    <Styles.Container source={BackgroundImage}>
+      <Styles.Title>Ignite Fleet</Styles.Title>
+
+      <Styles.Slogan>Gestão de uso de veículos</Styles.Slogan>
+
+      <Button
+        title="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoolgeSignin}
+      />
+    </Styles.Container>
+  );
 };
 
 export default SignIn;
